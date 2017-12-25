83ac68d8e3b96d90ca45c67b587943f95ca66c72
Jayfrown <info@jayfrown.nl>
2017-12-25 13:37:07 +0100

    cleanup | some small changes

--- pycor/interact.py
+++ pycor/interact.py
@@ -8,49 +8,43 @@ from __future__ import print_function
 import sys
 import textwrap
 
-from termcolor import colored
-
+try:
+    from termcolor import colored
+except Exception:
+    def colored(args, bogus):
+        return args
 
 # message types
 def gMsg(args, **kwargs):
     print(colored("pycor", 'blue') + ':', args, file=sys.stdout, **kwargs)
-
-
 def bMsg(args, **kwargs):
-    print(colored("pycor", 'red') + ':', args, file=sys.stderr, **kwargs)
-
+    print(colored("pycor",  'red') + ':', args, file=sys.stderr, **kwargs)
 
 # program header
 def print_header():
-    gMsg(colored("Container", 'magenta') + "/" + colored("Overlay", 'magenta'))
-    gMsg("leverage overlayfs with lxd containers")
+    print(colored("Container", 'magenta') + "/" + colored("Overlay", 'magenta'))
+    print("leverage overlayfs with lxd containers")
     print()
-
 
 # usage hints
 def print_usage():
     print("usage: " + sys.argv[0] + " [-h|--help] action [args]")
 
-
 def full_usage():
     print_usage()
     print("available actions:")
-
     print(textwrap.dedent("""
     {containers}:
-        launch [{name}]        - new container
-        delete  {name}         - delete container
+        launch [{name}]        - new overlain container
+        delete  {name}         - umount and delete container
     """).format(name=colored("name", 'magenta'),
-                containers=colored("containers", 'blue')))
-
+          containers=colored("containers", 'blue')))
 
 # help messages
 def print_help(topic):
     if topic == "launch":
         print("help for launch")
-
     elif topic == "delete":
         print("help for delete")
-
     else:
-        raise RuntimeError("unknown topic: " + str(topic))
+        raise RuntimeError("unknown action: " + str(topic))
