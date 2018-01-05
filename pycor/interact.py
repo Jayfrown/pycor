3409cffde9844c7504d5237e67f2872ede870b0e
Jayfrown <info@jayfrown.nl>
2018-01-05 15:27:34 +0100

    ux | drop colored output
    additionally rewrite some usage logic

--- pycor/interact.py
+++ pycor/interact.py
@@ -8,37 +8,28 @@ from __future__ import print_function
 import sys
 import textwrap
 
-try:
-    from termcolor import colored
-except Exception:
-    def colored(string, bogus):
-        return string
 
 # message types
-def gMsg(string, **kwargs):
-    print(colored("pycor", 'blue') + ':', string, file=sys.stdout, **kwargs)
-def bMsg(string, **kwargs):
-    print(colored("pycor",  'red') + ':', string, file=sys.stderr, **kwargs)
+def gMsg(string):
+    print("pycor: ", string, file=sys.stdout)
+def bMsg(string):
+    print("pycor: ", string, file=sys.stderr)
 
 # program header
 def print_header():
-    print(colored("Container", 'magenta') + "/" + colored("Overlay", 'magenta'))
+    print("Container/Overlay")
     print("leverage overlayfs with lxd containers")
     print()
 
 # usage hints
 def print_usage():
     print("usage: " + sys.argv[0] + " [-h|--help] action [args]")
-
-def full_usage():
-    print_usage()
     print("available actions:")
     print(textwrap.dedent("""
-    {containers}:
-        launch [{name}]        - new overlain container
-        delete  {name}         - umount and delete container
-    """).format(name=colored("name", 'magenta'),
-          containers=colored("containers", 'blue')))
+    containers:
+        launch [name]        - new overlain container
+        delete  name         - umount and delete container
+    """))
 
 # help messages
 def print_help(topic):
