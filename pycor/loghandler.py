57da6b4a0667a13f119d5bcb581a05091a6dad38
Jayfrown <info@jayfrown.nl>
2018-01-13 23:20:56 +0100

    Add licensing information

--- pycor/loghandler.py
+++ pycor/loghandler.py
@@ -2,6 +2,25 @@
 # pycor/logging.py
 #    handle log messages
 #
+#   This file is a part of the pycor project. To obtain the latest
+#   development version, head over to the git repository available
+#   at github: https://github.com/Jayfrown/pycor
+#
+#   Copyright (C) 2018  Jeroen de Boer (info@jayfrown.nl)
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 import logging
 logger = logging.getLogger()
--- pycor/optparser.py
+++ pycor/optparser.py
@@ -1,9 +1,33 @@
 ##
 # pycor/docopt.py
 #    use docopt to create a stable cli
+#
+#   This file is a part of the pycor project. To obtain the latest
+#   development version, head over to the git repository available
+#   at github: https://github.com/Jayfrown/pycor
+#
+#   Copyright (C) 2018  Jeroen de Boer (info@jayfrown.nl)
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 """Container/Overlay
 leverage overlayfs with lxd containers
+
+pycor   Copyright (C) 2018   Jeroen de Boer (info@jayfrown.nl)
+This program is free software; you are free to use, modify and/or
+redistribute it under certain conditions. See LICENSE for details.
 
 usage: pycor-cli [-d|--debug] [-h|--help] <action> [<args> ...]
 
